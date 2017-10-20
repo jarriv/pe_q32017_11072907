21b97d0a6203ab783b6823d014c5ee0e4c74eef1
jarriv <32197247+jarriv@users.noreply.github.com>
2017-10-20 00:40:58 -0200

    Add files via upload

---
+++ lista03/6.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmpfunc(const void *a, const void *b)
+{
+	return (*(int *)a - *(int *)b);
+}
+
+int main(void) {
+	double mediana;
+	int n,*v;
+	scanf("%d",&n);
+	v = malloc(n*sizeof(int));
+
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&v[i]);
+	}
+
+	qsort(v, n, sizeof(int), cmpfunc);
+
+	if(n%2!=0)
+	{
+		mediana=v[n/2];
+	}
+	else
+	{
+		mediana=(v[n/2-1]+v[n/2])/2.0;
+	}
+
+	printf("%.2lf",mediana);
+
+	return 0;
+}
